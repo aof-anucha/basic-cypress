c85873c9e746882713872f7d4707e33af3621ec6
aof-anucha <anucha.wilailert@gmail.com>
2024-05-30 19:21:14 +0700

    first commit

---
+++ cypress/e2e/1-getting-started/check_time_movie.cy.js
@@ -0,0 +1,12 @@
+const url = 'https://www.sfcinemacity.com'
+const todayDate = Cypress.moment().format('DD MMM YYYY')
+const nowTime = Cypress.moment().format('HH:mm')
+const expectTime = Cypress.moment().add(1, 'hours').format('HH:mm')
+const nameMovie = 'Homestay'
+const locationMovie = 'SFX CINEMA Central Rama 9'
+
+describe('Check Time Movie', () => {
+    it('Go to url', () => {
+       cy.visit(url)
+    })
+  })
