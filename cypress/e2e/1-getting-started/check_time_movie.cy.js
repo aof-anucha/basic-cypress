d965afe788a2ea476bdcab77e1eb4b21c25aacaf
aof-anucha <anucha.wilailert@gmail.com>
2024-06-01 02:10:15 +0700

    add change language SF

--- cypress/e2e/1-getting-started/check_time_movie.cy.js
+++ cypress/e2e/1-getting-started/check_time_movie.cy.js
@@ -1,12 +1,19 @@
+const dayjs = require('dayjs');
 const url = 'https://www.sfcinemacity.com'
-const todayDate = Cypress.moment().format('DD MMM YYYY')
-const nowTime = Cypress.moment().format('HH:mm')
-const expectTime = Cypress.moment().add(1, 'hours').format('HH:mm')
+const todayDate = dayjs().format('DD MMM YYYY');
+const nowTime = dayjs().format('HH:mm')
+const expectTime = dayjs().add(1, 'hours').format('HH:mm')
 const nameMovie = 'Homestay'
 const locationMovie = 'SFX CINEMA Central Rama 9'
 
 describe('Check Time Movie', () => {
     it('Go to url', () => {
        cy.visit(url)
+
+       cy.contains('a', 'ENG').click();
+
+       cy.contains('a', 'Login/Sign up').should('exist');
+
+       cy.get('.signin a').should('contain.text', "Login/Sign up")
     })
   })
